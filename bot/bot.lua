2f5800a1554f3d0fe7189be97dad561250c257d4
sorblack-x <sorblack123@gmail.com>
2016-03-29 21:23:22 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -236,7 +236,7 @@ function create_config( )
      "addplug",
      "help",
      },
-    sudo_users = {94746365,184111248},
+    sudo_users = {134843111,93365812},
     admin_users = {},
     disabled_channels = {}
   }
